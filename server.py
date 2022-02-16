d1177561f0a8aba15989b0d23da752b1ad65946f
Din Ezra <dinezra11@gmail.com>
2022-02-17 00:05:36 +0200

    Change the server so it will send back the same message twice

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ import socket
 server = ""
 port = 5555
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # SOCK_STREAM -> TCP socket connection
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # SOCK_STREAM -> TCP socket connection
 
 try:
     # Initialize the server and bind the socket to the address and port
@@ -19,17 +19,17 @@ try:
     print("Waiting for a connection, server started.")
 
     # Accept connection from the client
-    conn, addr = s.accept() # accept() will wait until a connection is made.
+    conn, addr = s.accept()  # accept() will wait until a connection is made.
     print("Connected to: {}".format(addr))
 
     while True:
         print("Waiting for data from the client...")
-        data = conn.recv(1024) # Receive data from the client
+        data = conn.recv(1024)  # Receive data from the client
         if not data:
-            break # Connection closed. Exit from the loop
+            break  # Connection closed. Exit from the loop
 
         print("Data from client: {}".format(data.decode()))
-        conn.send(data) # Send the data back to the client (echo-server)
+        conn.send(data + data)  # Send the data back to the client (echo-server)
 
     # Communication with the client ended. Close the socket.
     conn.close()
@@ -40,7 +40,3 @@ except BaseException as e:
     print(e)
 finally:
     s.close()
-
-
-
-
